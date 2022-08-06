2152f78cab438d13e957ad2d856574a5a7cce7a0
AikAcrobatt <locked_gold_box@protonmail.ch>
2022-08-07 01:11:42 +0400

    Algorithms unit_tests (1)

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -6,9 +6,24 @@
 
 //BETTER ENUMS
 //a whole-declarator of better_enums
-#define UNS_ENUM_DECLARATOR(ENUM_NAME, SPECIFICATION_TYPE, ...)																	            \
-	BETTER_ENUM(ENUM_NAME, SPECIFICATION_TYPE, __VA_ARGS__);															                            \
-	bool operator==(const ENUM_NAME& arg1, const ENUM_NAME::_enumerated& arg2) { return arg1 == static_cast<ENUM_NAME>(arg2); };			\
-	bool operator==(const ENUM_NAME::_enumerated& arg1, const ENUM_NAME& arg2) { return static_cast<ENUM_NAME>(arg1) == arg2; };			\
-	bool operator!=(const ENUM_NAME& arg1, const ENUM_NAME::_enumerated& arg2) { return !(arg1 == static_cast<ENUM_NAME>(arg2)); };			\
-	bool operator!=(const ENUM_NAME::_enumerated& arg1, const ENUM_NAME& arg2) { return !(static_cast<ENUM_NAME>(arg1) == arg2); };		
+#define UNS_BENUM_DECLARATOR(BENUM_NAME, SPECIFICATION_TYPE, ...)																	        \
+	BETTER_ENUM(BENUM_NAME, SPECIFICATION_TYPE, __VA_ARGS__);															                    \
+	bool operator==(const BENUM_NAME& arg1, const BENUM_NAME::_enumerated& arg2) { return arg1 == static_cast<BENUM_NAME>(arg2); };			\
+	bool operator==(const BENUM_NAME::_enumerated& arg1, const BENUM_NAME& arg2) { return static_cast<BENUM_NAME>(arg1) == arg2; };			\
+	bool operator!=(const BENUM_NAME& arg1, const BENUM_NAME::_enumerated& arg2) { return !(arg1 == static_cast<BENUM_NAME>(arg2)); };		\
+	bool operator!=(const BENUM_NAME::_enumerated& arg1, const BENUM_NAME& arg2) { return !(static_cast<BENUM_NAME>(arg1) == arg2); };		
+
+
+namespace uns {
+	template<typename benum_t>
+	concept is_benum = requires(benum_t enum_obj) {
+		benum_t::_values();
+		benum_t::_values().begin();
+		benum_t::_values().end();
+		benum_t::_enumerated;
+		benum_t::_value_iterator;
+		benum_t::_value_iterator::operator*();
+		benum_t::_to_string();
+		benum_t::_from_string();
+	};
+};
---
+++ algorithms/test.cpp
@@ -0,0 +1,61 @@
+﻿
+
+#pragma warning(disable: 4668; disable: 4365)
+#include <iostream>
+#pragma warning(default: 4668; default: 4365)
+
+#define BOOST_TEST_MODULE uns::algorithms::test
+#include "boost/test/included/unit_test.hpp"
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#pragma warning(disable: 4514)
+#include "algorithms.h"
+
+namespace abyss {
+
+    UNS_BENUM_DECLARATOR(layers, int,
+        l0 = 0,
+        l1 = 1,
+        l2 = 2,
+        l3 = 3,
+        l4 = 4,
+        l5 = 5,
+        l6 = 6,
+        l7 = 7,
+        l8 = 8,
+        l9 = 9
+    );
+
+};
+#pragma warning(default: 4514)
+
+
+template<uns::is_benum benum_t>
+class benum_dataset_generator {
+public:
+    enum {
+        arity = 1
+    };
+
+    boost::unit_test::data::size_t size() const { return benum_t::_values().size(); };
+
+    benum_t::_value_iterator begin() { return benum_t::_values().begin(); };
+    benum_t::_value_iterator end() { return benum_t::_values().begin(); };
+};
+
+
+BOOST_AUTO_TEST_CASE(benum_equality_test0) {
+    auto l3 = static_cast<abyss::layers>(abyss::layers::l3);
+    BOOST_TEST(l3 == abyss::layers::l3);
+};
+
+
+BOOST_DATA_TEST_CASE(benum_equality_test1,
+    benum_dataset_generator<abyss::layers>() ^ boost::unit_test::data::make({ abyss::layers::l0, abyss::layers::l1, abyss::layers::l2, abyss::layers::l3, abyss::layers::l4, abyss::layers::l5, abyss::layers::l6, abyss::layers::l7, abyss::layers::l8, abyss::layers::l9 }),
+    //boost::unit_test::data::make({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }) ^ boost::unit_test::data::make({ abyss::layers::l0, abyss::layers::l1, abyss::layers::l2, abyss::layers::l3, abyss::layers::l4, abyss::layers::l5, abyss::layers::l6, abyss::layers::l7, abyss::layers::l8, abyss::layers::l9 }),
+    //benum_dataset_generator<abyss::layers>(),
+    true_benum_obj, benum_id
+) {
+    BOOST_TEST(true_benum_obj == benum_id);
+};
