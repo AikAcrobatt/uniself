bd82af88df3c9289a33b0e366fe70c691ba22e92
AikAcrobatt <locked_gold_box@protonmail.ch>
2022-09-25 23:25:18 +0400

    math testing unfinished (1)

--- math/test.cpp
+++ math/test.cpp
@@ -1,5 +1,6 @@
 ﻿
 #include <iostream>
+#include <limits>
 
 #include "uniself/math.h"
 
@@ -13,24 +14,39 @@
 
 #pragma warning(disable: 4514)
 
-
+#undef min
+auto minw = std::numeric_limits<long double>::min();
+constexpr auto eq_modifier = 1.0e-14L;
 
 BOOST_AUTO_TEST_SUITE(equals_test)
 
     BOOST_DATA_TEST_CASE(correct1,
-        boost::unit_test::data::xrange(0.1L, (boost::unit_test::data::begin = -0.1L, boost::unit_test::data::step = 0.001L))
-        * boost::unit_test::data::xrange(0.1L, (boost::unit_test::data::begin = -0.1L, boost::unit_test::data::step = 0.001L)),
+        boost::unit_test::data::xrange(10.0L * minw, (boost::unit_test::data::begin = -10.0L * minw, boost::unit_test::data::step = minw))
+        * boost::unit_test::data::xrange(10.0L * minw, (boost::unit_test::data::begin = -10.0L * minw, boost::unit_test::data::step = minw)),
         val1, val2
     ) {
-        BOOST_TEST(uns::math::equals(val1, val2) == (uns::math::abs(val1 - val2) < 0.000000001));
+        BOOST_TEST(uns::math::equals(val1, val2));
+    };
+
+    BOOST_DATA_TEST_CASE(incorrect1,
+        (boost::unit_test::data::xrange(10.0L * minw, (boost::unit_test::data::begin = -10.0L * minw, boost::unit_test::data::step = minw))
+            * boost::unit_test::data::xrange((-10.0L - 0.01) * minw, (boost::unit_test::data::begin = -100.0L * minw, boost::unit_test::data::step = minw)))
+        + (boost::unit_test::data::xrange(10.0L * minw, (boost::unit_test::data::begin = -10.0L * minw, boost::unit_test::data::step = minw))
+            * boost::unit_test::data::xrange(100.0L * minw, (boost::unit_test::data::begin = (10.0L + 0.01) * minw, boost::unit_test::data::step = minw))),
+        val1, val2
+    ) {
+        BOOST_TEST(!uns::math::equals(val1, val2));
+
+        BOOST_TEST(!uns::math::equals(val2, val1));
     };
 
     BOOST_DATA_TEST_CASE(correct2,
-        boost::unit_test::data::xrange(3.1L, (boost::unit_test::data::begin = 2.9L, boost::unit_test::data::step = 0.001L))
-        * boost::unit_test::data::xrange(10, (boost::unit_test::data::begin = -10, boost::unit_test::data::step = 1)),
-        val1, val2
+        boost::unit_test::data::xrange(1.0L, (boost::unit_test::data::begin = 1000.0L, boost::unit_test::data::step = 1.0L))
+        * boost::unit_test::data::xrange(2.0L * eq_modifier / (1.0L - eq_modifier), (boost::unit_test::data::begin = -2.0L * eq_modifier / (1.0L + eq_modifier), boost::unit_test::data::step = eq_modifier / 100.0L)),
+        val1, mult
     ) {
-        BOOST_TEST(uns::math::equals(val1, val2) == (uns::math::abs(val1 - static_cast<long double>(val2)) < 0.000000001));
+        BOOST_TEST(uns::math::equals(val1, val1 * (1 + mult)));
     };
+
 
 BOOST_AUTO_TEST_SUITE_END();
