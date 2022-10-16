6888f4924d68ea5c541f5385f1f2d72c0cfb3baa
AikAcrobatt <locked_gold_box@protonmail.ch>
2022-10-16 19:02:29 +0400

    Some concepts added

--- concepts.h
+++ concepts.h
@@ -4,14 +4,19 @@
 
 namespace uns {
 
+	template<typename from_t, typename to_t>
+	concept constructible_to = std::constructible_from<from_t, to_t>;
+
 	template<typename collection_t, typename value_t>
-	concept collection = requires (collection_t collection) {
-		collection.begin();
-		collection.end();
-		{ *(collection.begin()) } -> std::convertible_to<value_t>;
-		collection.begin()++;
-		{ collection.size() } -> std::integral;
-	};
+	concept collection = 
+		std::same_as<value_t, typename collection_t::value_type>
+		&& requires (collection_t collection) {
+			collection.begin();
+			collection.end();
+			{ *(collection.begin()) } -> std::convertible_to<value_t>;
+			collection.begin()++;
+			{ collection.size() } -> std::integral;
+		};
 
 	template<typename collection_t, typename value_t>
 	concept const_collection =
@@ -31,5 +36,22 @@ namespace uns {
 	template<typename collection_t, typename target_convertion_t>
 	concept const_collection_of_convertibles_to =
 		uns::const_collection<collection_t, typename collection_t::value_type> && std::convertible_to<typename collection_t::value_type, target_convertion_t>;
+
+	template<typename collection_t, typename target_convertion_t>
+	concept collection_of_constructibles_from =
+		uns::collection<collection_t, typename collection_t::value_type> && std::constructible_from<typename collection_t::value_type, target_convertion_t>;
+
+	template<typename collection_t, typename target_convertion_t>
+	concept const_collection_of_constructibles_from =
+		uns::const_collection<collection_t, typename collection_t::value_type> && std::constructible_from<typename collection_t::value_type, target_convertion_t>;
+
+	template<typename collection_t, typename target_convertion_t>
+	concept collection_of_constructibles_to =
+		uns::collection<collection_t, typename collection_t::value_type> && uns::constructible_to<typename collection_t::value_type, target_convertion_t>;
+
+	template<typename collection_t, typename target_convertion_t>
+	concept const_collection_of_constructibles_to =
+		uns::const_collection<collection_t, typename collection_t::value_type> && uns::constructible_to<typename collection_t::value_type, target_convertion_t>;
+
 
 };
--- strings/test.cpp
+++ strings/test.cpp
@@ -883,11 +883,11 @@ BOOST_AUTO_TEST_SUITE(string_seeker_methods)
         };
 
         BOOST_AUTO_TEST_CASE(incorrect_6) {
-            auto target = std::u8string{ u8"" };
-            auto sample = std::u8string{ u8" ру" };
+            auto target = uns::test::make_u8(u8"");
+            auto sample = uns::test::make_u8(u8" py");
 
             BOOST_TEST(
-                static_cast<int>(target.cend() - target.cbegin()) == uns::string::find<std::u8string>(target, target.cbegin(), sample) - target.cbegin()
+                static_cast<int>(target.cend() - target.cbegin()) == uns::string::find<uns::test::u8string_wrapper>(target, target.cbegin(), sample) - target.cbegin()
             );
         };
 
@@ -896,7 +896,7 @@ BOOST_AUTO_TEST_SUITE(string_seeker_methods)
             auto sample = uns::test::make_u8(u8"");
 
             BOOST_TEST(
-                static_cast<int>(target.cend() - target.cbegin()) == uns::string::find<std::u8string>(target, target.cbegin(), sample) - target.cbegin()
+                static_cast<int>(target.cend() - target.cbegin()) == uns::string::find<uns::test::u8string_wrapper>(target, target.cbegin(), sample) - target.cbegin()
             );
         };
 
@@ -1007,11 +1007,11 @@ BOOST_AUTO_TEST_SUITE(string_seeker_methods)
                 "some_key3"
             };
 
-            auto pos_of_sample1 = uns::string::find(target, target.begin(), static_cast<std::string>("some_key1"));
+            auto pos_of_sample1 = uns::string::find<std::string>(target, target.begin(), "some_key1");
             auto seeker = target.begin();
 
             BOOST_TEST(
-                true == uns::string::seeker_set(target, seeker, samples, true, 3)
+                true == uns::string::seeker_set<std::string>(target, seeker, samples, true, 3)
             );
 
             BOOST_TEST(
