3a825ec2d8a9c7d018c9f3b016ea8752d1a8ca4e
AikAcrobatt <locked_gold_box@protonmail.ch>
2022-11-03 22:32:42 +0400

    A 'neuronetworks' proj created

---
+++ neuronetworks/test.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <limits>
+
+#include "uniself/neuronetworks.h"
+
+#pragma warning(default: 4668; default: 4365)
+
+#define BOOST_TEST_MODULE uns_neuronetworks
+#include "boost/test/included/unit_test.hpp"
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/mpl/list.hpp>
+
+#pragma warning(disable: 4514)
+
+
+BOOST_AUTO_TEST_SUITE(neuronetworks_str_test)
+
+    BOOST_AUTO_TEST_CASE(correct1) {
+
+        BOOST_TEST(true);
+    };
+
+BOOST_AUTO_TEST_SUITE_END();
