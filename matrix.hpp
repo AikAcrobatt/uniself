4d632852481011b422890435f400d701578a8961
AikAcrobatt <locked_gold_box@protonmail.ch>
2025-03-09 17:56:58 +0400

    At 'matrix' lib: some cosmetic changes

--- matrix.hpp
+++ matrix.hpp
@@ -77,9 +77,9 @@ namespace uns::math {
 namespace uns::string {
 
 	//casts to & from u8strings for matrices
-	template<::std::convertible_to<::std::u8string> out_t,::uns::math::ublas_matrix matrix_t>
+	template<::std::convertible_to<::std::u8string> out_t, ::uns::math::ublas_matrix matrix_t>
 	out_t u8_cast(const matrix_t& mtx) {
-		auto res = static_cast<::std::u8string>(u8"[");
+		auto res = ::std::u8string{ u8"[" };
 		res += ::uns::string::u8_cast<::std::u8string>(mtx.size1());
 		res += u8" x ";
 		res += ::uns::string::u8_cast<::std::u8string>(mtx.size2());
@@ -88,22 +88,26 @@ namespace uns::string {
 		auto i1 = typename matrix_t::size_type{ 0 };
 		auto i2 = typename matrix_t::size_type{ 0 };
 
-		auto is_upmost = true;
-		auto is_leftmost = true;
-		for(i1 = 0; i1 < mtx.size1(); i1++) {
-			if(!is_upmost)
+		bool is_upmost = true;
+		bool is_leftmost = true;
+		for(i1 = 0; i1 < mtx.size1(); ++i1) {
+			if(!is_upmost) {
 				res += u8",\n";
-			else
+			}
+			else {
 				is_upmost = false;
+			};
 
 			res += u8"\t{";
 
 			is_leftmost = true;
-			for(i2 = 0; i2 < mtx.size2(); i2++) {
-				if(!is_leftmost)
+			for(i2 = 0; i2 < mtx.size2(); ++i2) {
+				if(!is_leftmost) {
 					res += u8", ";
-				else
+				}
+				else {
 					is_leftmost = false;
+				};
 
 				res += ::uns::string::u8_cast<::std::u8string>(mtx(i1, i2));
 			};
@@ -117,13 +121,13 @@ namespace uns::string {
 	};
 	template<::uns::math::ublas_matrix matrix_t>
 	matrix_t u8_cast(const ::std::u8string& str) {
-		static const auto bracket_op = ::std::u8string{ u8"[" };
-		static const auto mul = ::std::u8string{ u8"x" };
-		static const auto bracket_cl = ::std::u8string{ u8"]" };
-		static const auto space = ::std::u8string{ u8" " };
-		static const auto comma = ::std::u8string{ u8"," };
-		static const auto brace_op = ::std::u8string{ u8"{" };
-		static const auto brace_cl = ::std::u8string{ u8"}" };
+		constexpr auto bracket_op = ::std::u8string{ u8"[" };
+		constexpr auto mul = ::std::u8string{ u8"x" };
+		constexpr auto bracket_cl = ::std::u8string{ u8"]" };
+		constexpr auto space = ::std::u8string{ u8" " };
+		constexpr auto comma = ::std::u8string{ u8"," };
+		constexpr auto brace_op = ::std::u8string{ u8"{" };
+		constexpr auto brace_cl = ::std::u8string{ u8"}" };
 
 		auto i1 = typename matrix_t::size_type{ 0 };
 		auto i2 = typename matrix_t::size_type{ 0 };
@@ -132,34 +136,56 @@ namespace uns::string {
 		auto read_str = ::std::u8string{};
 		auto seeker = str.begin();
 
-		if(!::uns::string::seeker_set(str, seeker, bracket_op, false, -1)) throw ::std::runtime_error{"Matrix string format violation: a '[' not found"};
-		if(!::uns::string::seeker_read(str, seeker, read_str, mul, false, -1)) throw ::std::runtime_error{ "Matrix string format violation: cant read till 'x'" };
+		if(!::uns::string::seeker_set(str, seeker, bracket_op, false, -1)) {
+			throw ::std::runtime_error{ "Matrix string format violation: a '[' not found" };
+		};
+		if(!::uns::string::seeker_read(str, seeker, read_str, mul, false, -1)) {
+			throw ::std::runtime_error{ "Matrix string format violation: cant read till 'x'" };
+		};
 		auto size1 = ::uns::string::u8_cast<typename matrix_t::size_type>(read_str);
-		if(!::uns::string::seeker_read(str, seeker, read_str, bracket_cl, false, -1)) throw ::std::runtime_error{ "Matrix string format violation: cant read till ']'" };
+		if(!::uns::string::seeker_read(str, seeker, read_str, bracket_cl, false, -1)) {
+			throw ::std::runtime_error{ "Matrix string format violation: cant read till ']'" };
+		};
 		auto size2 = ::uns::string::u8_cast<typename matrix_t::size_type>(read_str);
 		res.resize(size1, size2);
 
-		if(!::uns::string::seeker_set(str, seeker, brace_op, false, -1)) throw ::std::runtime_error{ "Matrix string format violation: a '{' not found" };
+		if(!::uns::string::seeker_set(str, seeker, brace_op, false, -1)) {
+			throw ::std::runtime_error{ "Matrix string format violation: a '{' not found" };
+		};
 
-		for(i1 = 0; i1 < res.size1(); i1++) {
-			if(!::uns::string::seeker_set(str, seeker, brace_op, false, -1)) throw ::std::runtime_error{ "Matrix string format violation: a newline '{' not found" };
+		for(i1 = 0; i1 < res.size1(); ++i1) {
+			if(!::uns::string::seeker_set(str, seeker, brace_op, false, -1)) {
+				throw ::std::runtime_error{ "Matrix string format violation: a newline '{' not found" };
+			};
 
-			for(i2 = 0; i2 < res.size2(); i2++) {
+			for(i2 = 0; i2 < res.size2(); ++i2) {
 				if(i2 < res.size2() - 1) {
-					if(!::uns::string::seeker_read(str, seeker, read_str, comma, false, -1, brace_cl)) throw ::std::runtime_error{ "Matrix string format violation: cant read till ',' of dim #2" };
-					else res(i1, i2) = ::uns::string::u8_cast<typename matrix_t::value_type>(read_str);
+					if(!::uns::string::seeker_read(str, seeker, read_str, comma, false, -1, brace_cl)) {
+						throw ::std::runtime_error{ "Matrix string format violation: cant read till ',' of dim #2" };
+					}
+					else {
+						res(i1, i2) = ::uns::string::u8_cast<typename matrix_t::value_type>(read_str);
+					};
 				}
 				else {
-					if(!::uns::string::seeker_read(str, seeker, read_str, brace_cl, false, -1)) throw ::std::runtime_error{ "Matrix string format violation: cant read till  '}' of dim #2" };
-					else res(i1, i2) = ::uns::string::u8_cast<typename matrix_t::value_type>(read_str);
+					if(!::uns::string::seeker_read(str, seeker, read_str, brace_cl, false, -1)) {
+						throw ::std::runtime_error{ "Matrix string format violation: cant read till  '}' of dim #2" };
+					}
+					else {
+						res(i1, i2) = ::uns::string::u8_cast<typename matrix_t::value_type>(read_str);
+					};
 				};
 
 			};
 			if(i1 < res.size1() - 1) {
-				if(!::uns::string::seeker_set(str, seeker, comma, false, -1, brace_cl)) throw ::std::runtime_error{ "Matrix string format violation: a ',' of dim #1 not found" };
+				if(!::uns::string::seeker_set(str, seeker, comma, false, -1, brace_cl)) {
+					throw ::std::runtime_error{ "Matrix string format violation: a ',' of dim #1 not found" };
+				};
 			}
 			else {
-				if(!::uns::string::seeker_set(str, seeker, brace_cl, false, -1)) throw ::std::runtime_error{ "Matrix string format violation: a '}' of dim #1 not found" };
+				if(!::uns::string::seeker_set(str, seeker, brace_cl, false, -1)) {
+					throw ::std::runtime_error{ "Matrix string format violation: a '}' of dim #1 not found" };
+				};
 			};
 		};
 
@@ -457,8 +483,8 @@ namespace uns::math {
 		auto i1 = static_cast<typename matrix_t::size_type>(0);
 		auto i2 = static_cast<typename matrix_t::size_type>(0);
 
-		for(i1 = 0; i1 < m.size1(); i1++) {
-			for(i2 = 0; i2 < m.size2(); i2++) {
+		for(i1 = 0; i1 < m.size1(); ++i1) {
+			for(i2 = 0; i2 < m.size2(); ++i2) {
 				res(i2, i1) = m(i1, i2);
 			};
 		};
