963762846e92222da572b7e0c52d7fdb74e6012a
AikAcrobatt <locked_gold_box@protonmail.ch>
2026-04-27 19:16:07 +0400

    New lib 'calendar': lib 'datetime' was removed

--- datetime.hpp
+++ datetime.hpp
@@ -2,6 +2,8 @@
 
 #include <chrono>
 
+#include "uniself/renum.hpp"
+
 #ifndef UNS_LIB_DATETIME
 #define UNS_LIB_DATETIME
 
@@ -38,6 +40,26 @@ namespace uns {
     };
 
     ::uns::datetime::time_point timestamp() noexcept;
+};
+
+
+namespace uns::gregorian {
+
+    UNS_RENUM(month, uint8_t
+        , (january, = 1)
+        , (february, = 2)
+        , (march, = 3)
+        , (april, = 4)
+        , (may, = 5)
+        , (june, = 6)
+        , (july, = 7)
+        , (august, = 8)
+        , (september, = 9)
+        , (october, = 10)
+        , (november, = 11)
+        , (december, = 12)
+    );
+
 };
 
 #endif
